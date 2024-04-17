70af4edc507dcf97645729084c9e4c5594dee981
Vasil Velinov <velinov.vasil.i@gmail.com>
2024-04-17 14:34:06 +0300

    Added GlobalErrorHandler

---
+++ src/main/java/com/projectmanager/projectmanagementapp/handler/GlobalErrorHandler.java
@@ -0,0 +1,9 @@
+package com.projectmanager.projectmanagementapp.handler;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalErrorHandler {
+
+
+}
